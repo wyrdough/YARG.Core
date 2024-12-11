288381f43d7a506b971ae2415fa152952bb43f63
wyrdough <nathan@nwacg.net>
2024-12-11 13:28:07 -0500

    Add timing information to SoloSection
    This allows TrackPlayer to calculate solos ahead of time without reading the note stream to find upcoming solo sections.

--- YARG.Core/Engine/SoloSection.cs
+++ YARG.Core/Engine/SoloSection.cs
@@ -6,11 +6,20 @@ namespace YARG.Core.Engine
         public int NoteCount { get; }
 
         public int NotesHit { get; set; }
-        
+
         public int SoloBonus { get; set; }
 
-        public SoloSection(int noteCount)
+        public uint   StartTick { get; private set; }
+        public uint   EndTick   { get; private set; }
+        public double StartTime { get; private set; }
+        public double EndTime   { get; private set; }
+
+        public SoloSection(uint start, uint end, double startTime, double endTime, int noteCount)
         {
+            StartTick = start;
+            EndTick = end;
+            StartTime = startTime;
+            EndTime = endTime;
             NoteCount = noteCount;
         }
 
