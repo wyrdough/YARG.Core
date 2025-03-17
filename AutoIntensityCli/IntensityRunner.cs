6ced979d4d9688fcc7e412a51e8464b65ea9f085
wyrdough <nathan@nwacg.net>
2025-03-17 00:36:17 -0400

    Improve threading

--- AutoIntensityCli/IntensityRunner.cs
+++ AutoIntensityCli/IntensityRunner.cs
@@ -9,13 +9,17 @@ namespace AutoIntensityCli;
 
 public class IntensityRunner
 {
-    private static readonly string                         SongDirsPath   = Path.Combine(Environment.CurrentDirectory, "console_songdirs.txt");
-    public const            string                         CHART_PATH_VAR = "TEST_CHART_PATH";
-    private                 SongCache                      _songCache;
-    private                 Thread                         _chartLoadingThread;
-    private                 Thread                         _chartProcessingThread;
-    private                 ConcurrentQueue<SongWithChart> _loadedCharts = new();
-    private StreamWriter _csvFile;
+    private static readonly int                            NumThreads              = (int) Math.Floor(Environment.ProcessorCount / 1.5);
+    private static readonly string                         SongDirsPath            = Path.Combine(Environment.CurrentDirectory, "console_songdirs.txt");
+    private                 SongCache                      _songCache              = null!;
+    private readonly        Thread[]                       _chartLoadingThreads    = new Thread[NumThreads];
+    private readonly        Thread[]                       _chartProcessingThreads = new Thread[NumThreads / 2];
+    // We aren't currently using multiple queues, but this makes it easy for testing
+    private readonly        ConcurrentQueue<SongWithChart>[] _loadedCharts         = new ConcurrentQueue<SongWithChart>[1];
+    private readonly        int[]                          _processedCharts        = new int[NumThreads / 2];
+    private                 StreamWriter                   _csvFile                = null!;
+
+    private readonly Dictionary<HashWrapper, List<SongEntry>>[] _songEntries = new Dictionary<HashWrapper,List<SongEntry>>[NumThreads];
 
     private struct SongWithChart
     {
@@ -25,7 +29,7 @@ public class IntensityRunner
 
     public void IntensityTest()
     {
-        ConsoleUtilities.WriteMenuHeader("Auto Intensity Test");
+        ConsoleUtilities.WriteMenuHeader($"Auto Intensity Test: {NumThreads + NumThreads * 0.5} Threads");
 
         string chartPath = ConsoleUtilities.PromptTextInput("Please enter a chart directory: ", (input) =>
         {
@@ -49,26 +53,77 @@ public class IntensityRunner
         WriteCsvHeader(_csvFile);
 
         _songCache = CacheLoader.LoadCache();
+        var allEntries = _songCache.Entries.ToList();
 
-        _chartLoadingThread = new Thread(LoadCharts) { IsBackground = true };
-        _chartProcessingThread = new Thread(ProcessAllCharts) { IsBackground = true };
-        _chartLoadingThread.Start();
-        _chartProcessingThread.Start();
-        _chartProcessingThread.Join();
+        for (var i = 0; i < _loadedCharts.Length; i++)
+        {
+            _loadedCharts[i] = new ConcurrentQueue<SongWithChart>();
+        }
+
+        for (var i = 0; i < NumThreads; i++)
+        {
+            _songEntries[i] = new Dictionary<HashWrapper, List<SongEntry>>();
+        }
+
+        for (var i = 0; i < allEntries.Count; i++)
+        {
+            _songEntries[i % NumThreads].Add(allEntries[i].Key, allEntries[i].Value);
+        }
+
+        for (var i = 0; i < _chartLoadingThreads.Length; i++)
+        {
+            var x = i;
+            _chartLoadingThreads[i] = new Thread(() => LoadCharts(x))
+            {
+                IsBackground = true,
+                Name = $"Chart Loader {x + 1}"
+            };
+            _chartLoadingThreads[i].Start();
+        }
+
+        for (var i = 0; i < _chartProcessingThreads.Length; i++)
+        {
+            var x = i;
+            _chartProcessingThreads[i] = new Thread(() => ProcessAllCharts(x)) { IsBackground = true, Name = $"Chart Processing Thread {x + 1}" };
+            _chartProcessingThreads[i].Start();
+        }
+
+        var timer = new Stopwatch();
+        timer.Start();
+
+        var sequence = @"/-\|";
+        var counter = 0;
+        Console.Write("Processing charts: ");
+        // The chart processing threads won't end until the loading threads end, so we can just wait for them
+        while (_chartProcessingThreads.Any(x => x.IsAlive))
+        {
+            var symbol = sequence[counter % 4];
+            Console.Write(symbol);
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            counter++;
+            Thread.Yield();
+        }
+
+        timer.Stop();
+        var totalProcessed = _processedCharts.Sum();
+        Console.WriteLine($"Total charts processed: {totalProcessed} in {timer.ElapsedMilliseconds}ms");
 
         _csvFile.Close();
     }
 
-    private void ProcessAllCharts()
+    private bool ThreadsAreAlive => (_chartLoadingThreads.Any(x => x.IsAlive));
+
+    private void ProcessAllCharts(int threadNumber)
     {
+        int queueNum = threadNumber % _loadedCharts.Length;
         int chartCount = 0;
         int failedCount = 0;
         var timer = new Stopwatch();
         timer.Start();
 
-        while (_chartLoadingThread.IsAlive || !_loadedCharts.IsEmpty)
+        while (ThreadsAreAlive || !_loadedCharts[queueNum].IsEmpty)
         {
-            if (!_loadedCharts.TryDequeue(out var chart))
+            if (!_loadedCharts[queueNum].TryDequeue(out var chart))
             {
                 Thread.Sleep(1);
                 continue;
@@ -79,13 +134,13 @@ public class IntensityRunner
         }
         timer.Stop();
 
-        Console.WriteLine($"Processed {chartCount} charts in {timer.ElapsedMilliseconds} ms.");
+        _processedCharts[threadNumber] = chartCount;
+
+        // Console.WriteLine($"Processed {chartCount} charts in {timer.ElapsedMilliseconds} ms.");
         if (failedCount > 0)
         {
             Console.WriteLine($"Failed to read {failedCount} charts.");
         }
-
-
     }
 
     private bool ProcessChart(SongEntry cacheEntry, SongChart songChart, StreamWriter csvFile)
@@ -118,14 +173,15 @@ public class IntensityRunner
         }
         csvFile.WriteLine(line);
 
-        Console.WriteLine($"Chart processing time for {cacheEntry.Artist}: {cacheEntry.Name}: {timer.ElapsedMilliseconds}ms");
+        // Console.WriteLine($"Chart processing time for {cacheEntry.Artist}: {cacheEntry.Name}: {timer.ElapsedMilliseconds}ms");
         return true;
     }
 
-    private void LoadCharts()
+    private void LoadCharts(int threadNumber)
     {
+        var queueNum = threadNumber % _loadedCharts.Length;
         var loadTimer = new Stopwatch();
-        foreach (var node in _songCache.Entries.Values)
+        foreach (var node in _songEntries[threadNumber].Values)
         {
             foreach (var entry in node)
             {
@@ -139,7 +195,7 @@ public class IntensityRunner
                     Console.WriteLine($"Unable to load chart! ({entry.Artist}: {entry.Name})");
                     continue;
                 }
-                _loadedCharts.Enqueue(new SongWithChart {Entry = entry, Chart = songChart});
+                _loadedCharts[queueNum].Enqueue(new SongWithChart {Entry = entry, Chart = songChart});
             }
         }
     }
