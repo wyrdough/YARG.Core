52c064020ea341b7f9794002dd8b196adebfa972
wyrdough <nathan@nwacg.net>
2025-03-21 11:46:55 -0400

    Use upstream version of SoloSection.cs

--- YARG.Core/Engine/SoloSection.cs
+++ YARG.Core/Engine/SoloSection.cs
@@ -6,20 +6,11 @@
         public int NoteCount { get; }
 
         public int NotesHit { get; set; }
-
+        
         public int SoloBonus { get; set; }
 
-        public uint   StartTick { get; private set; }
-        public uint   EndTick   { get; private set; }
-        public double StartTime { get; private set; }
-        public double EndTime   { get; private set; }
-
-        public SoloSection(uint start, uint end, double startTime, double endTime, int noteCount)
+        public SoloSection(int noteCount)
         {
-            StartTick = start;
-            EndTick = end;
-            StartTime = startTime;
-            EndTime = endTime;
             NoteCount = noteCount;
         }
 
